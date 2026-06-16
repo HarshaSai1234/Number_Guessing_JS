70d16df0cfa843d473cc50af7333467f26319748
HarshaSai03 <2400030037@kluniversity.in>
2026-06-16 13:35:22 +0530

    Completed

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const submit = document.getElementById("submit");
 const result = document.getElementById("result");
 const attempt_score = document.getElementById("attempt_score")
-let num = Math.floor((Math.random()*100)+1);
-let attempts=0;
-let loop = true;
-console.log(num);
-    submit.onclick = function(){
-            attempts++;
-            let guess = document.getElementById("guess").value;
-            guess=Number(guess);
-            if(guess === num){
-                result.textContent="You got it right!!!";
-                attempt_score.textContent=`Attempts = ${attempts}`;
-                loop=false;
-            }
-            else{
-                result.textContent = "Try again";
-                attempt_score.textContent=`Attempts = ${attempts}`;
-            }
+const play = document.getElementById("play");
+play.onclick = function(){
+    let loop = true;
+    let num = Math.floor((Math.random()*100)+1);
+    let attempts=0;
+    console.log(num);
+    while(loop){
+        attempts++;
+        let guess = window.prompt("Guess the number");
+        if(isNaN(guess)){
+            window.alert("Enter a valid number!");
+        }
+        if(guess<0||guess>100){
+            window.alert("Enter a number only between 1 and 100");
+        }
+        if(guess==num){
+            loop=false;
+            result.textContent="You guessed it right!!";
+            attempt_score.textContent=`You took ${attempts} attempts`;
+
+        }
+        if(guess<num){
+            window.alert("Too low guess again!");
+        }
+        if(guess>num){
+            window.alert("Too high guess again!");
+        }
+    
+
     }
+}
